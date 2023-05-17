aa857953ce0a2791751a1bc215d3c3bc4afd0a64
nedorub <nedorub99@gmail.com>
2023-05-17 22:53:26 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int summAccaunt = 200; //начальный счёт
+        int summReplenishment = 1_224; //сумма пополнения
+        int bonus; //бонусы
+        int result; // итоговая сумма
+
+        if (summReplenishment >= 1_000) {
+            bonus = summReplenishment / 100; //рассчитываете сумму бонуса
+        } else {
+            bonus = 0;
+        }
+        result = summReplenishment + summAccaunt + bonus;
+        System.out.println("Баланс - " + result);
+
+    }
+}
